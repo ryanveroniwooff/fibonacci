56ae011934ce6892e0a49f8e8f297a965c80b5ad
Ryan Wooff <ryanwooff@gmail.com>
2016-11-10 03:35:54 +0000

    Version 1.0

---
+++ lib/fib.rb
@@ -0,0 +1,23 @@
+def recursive_fib(n)
+  n == 0 || n==1 ? n : recursive_fib(n-1) + recursive_fib(n-2)
+end
+
+def iterative_fib(n)
+  table = [1,1]
+  if n == 0 || n == 1 || n == 2
+    return 1
+  else
+    n.times do
+      @fib_table = table[table.length-1] + (table[table.length - 2])
+      table << @fib_table
+    end
+  end
+  return table.last
+end
+
+require 'benchmark'
+num = 35
+Benchmark.bm do |x|
+  x.report("rec:") { recursive_fib(num) }
+  x.report("itr:")  { iterative_fib(num)  }
+end
